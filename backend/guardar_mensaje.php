61275ec94d410772bd4412a7c09ad16eed62418d
EdwardFriend <luzardoeduardo2000@gmail.com>
2026-02-26 22:37:45 -0500

    Agrego script backend para guardar mensajes en MySQL

---
+++ backend/guardar_mensaje.php
@@ -0,0 +1,17 @@
+<?php
+include("conexion.php");
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$sql = "INSERT INTO mensajes (nombre, correo, mensaje) VALUES ('$nombre', '$correo', '$mensaje')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Mensaje enviado correctamente";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
